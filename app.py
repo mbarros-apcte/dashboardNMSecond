6c98c1ce9a642aac80c32536af0fca95c701ff9a
Barros Pereira <MBarros@chacompanies.com>
2022-07-11 10:37:50 -0400

    Fixed import errors

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import logging
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 #import cufflinks as cf
 import plotly.express as px
 import dash_bootstrap_components as dbc
